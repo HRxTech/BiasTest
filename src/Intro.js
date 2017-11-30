739fb680495d59c2529883550453fbf109d98986
Azza Rojbi <arojbi@my.bcit.ca>
2017-11-29 16:40:59 -0800

    set up

---
+++ src/Intro.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class Intro extends Component {
+  render() {
+    return (
+      <div className="Intro">
+        <h1 className="Intro-title">Welcome to HRx Bias Test</h1>
+        <p className="Intro-text">
+        Scalable silo equal opportunity progress entrepreneur human-centered collaborative consumption initiative. 
+        The initiative changemaker impact NGO invest inclusion. NGO impact, leverage when society; movements society thought partnership. 
+        Issue outcomes theory of change think tank natural resources venture philanthropy her body her rights. Paradigm living a fully ethical 
+        life NGO sustainable then thought partnership. Mobilize disrupt save the world; indicators, catalyze, empower communities big data B-corp. 
+        Relief, social return on investment theory of change expose the truth best practices or rubric our work. 
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Intro;
