7857230d4c6b2a23919a4465ba2b576e57e9fc92
Parandis <parandis.t.saheli@gmail.com>
2017-12-01 20:42:51 -0800

    Header and Footer

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
-import HRx_logo from './HRx_logo.svg';
+import axios from 'axios';
+
 
 
 class Header extends Component {
+    constructor(props){
+        super(props);
+        this.state = {logo:'', icon:''}
+    }
+    componentWillMount(){
+        axios.get('https://images.contentful.com/4xbeshmjlgqs/6D9jqlo0CIukmyS6CAM4Uk/12584fff1ec639b2e92ff904c7f0126a/37685_HRx_logo_.svg')
+        .then((response) => {
+           this.setState({logo:response.data}); 
+        });
+    
+        axios.get('https://images.contentful.com/4xbeshmjlgqs/31ZiBpxq9OqwEyEIyy4OW2/7117ba82feb446563b17f9000ca711ba/question.svg')
+        .then((response) => {
+           this.setState({icon:response.data}); 
+        });
+    }
+
   render() {
-    return (
-        <img src={HRx_logo} className="HRx-logo" alt="logo" />
-    );
+        return(
+            <div>
+            <img src={this.state.logo} className="logo" alt="logo" />
+            <img src={this.state.icon} className="icon" alt="question" />
+            </div>
+        );
   }
 }
 
