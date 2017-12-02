6b0ad98e4f4ab2790053752eee50195162203ed5
Parandis <parandis.t.saheli@gmail.com>
2017-12-02 11:39:08 -0800

    header and footer

--- src/Header.js
+++ src/Header.js
@@ -9,22 +9,26 @@ class Header extends Component {
         this.state = {logo:'', icon:''}
     }
     componentWillMount(){
+
         axios.get('https://images.contentful.com/4xbeshmjlgqs/6D9jqlo0CIukmyS6CAM4Uk/12584fff1ec639b2e92ff904c7f0126a/37685_HRx_logo_.svg')
         .then((response) => {
+            console.log(response);
            this.setState({logo:response.data}); 
         });
     
         axios.get('https://images.contentful.com/4xbeshmjlgqs/31ZiBpxq9OqwEyEIyy4OW2/7117ba82feb446563b17f9000ca711ba/question.svg')
         .then((response) => {
+            console.log(response);
            this.setState({icon:response.data}); 
         });
     }
 
   render() {
+      const {logo,icon} = this.state;
         return(
             <div>
-            <img src={this.state.logo} className="logo" alt="logo" />
-            <img src={this.state.icon} className="icon" alt="question" />
+            <img src={logo} className="logo" alt="logo" />
+            <img src={icon} className="icon" alt="question" />
             </div>
         );
   }
