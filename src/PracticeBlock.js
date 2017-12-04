2c50fb94aac1f9a24e14ae00ff99c41326dc6e1b
Christel Chan <christelyhchan@gmail.com>
2017-12-04 14:48:45 -0800

    Got categories with Axios

--- src/PracticeBlock.js
+++ src/PracticeBlock.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
+const API_BASE_URL = 'https://cdn.contentful.com';
+const API_SPACE_ID = '4xbeshmjlgqs';
+const API_TOKEN = '3bfead8c496ebd173c5b896acee22b2a9011df359db822a91d34dffd90abea07'
+
 class App extends Component {
   constructor(props){
     super(props);
@@ -14,9 +18,10 @@ class App extends Component {
   // Function to get Current Test Block Data
   componentWillMount(){
     // Request all Practice Block Entries
-    axios.get('https://cdn.contentful.com/spaces/4xbeshmjlgqs/entries?access_token=3bfead8c496ebd173c5b896acee22b2a9011df359db822a91d34dffd90abea07&content_type=practiceBlock')
+    axios.get(`${API_BASE_URL}/spaces/${API_SPACE_ID}/entries?access_token=${API_TOKEN}&content_type=practiceBlock`)
           .then((response) => {
 
+              console.log(response);
               // Get Current Practice Block Data
               var currentPracticeBlock = response.data.items[this.state.currentPracticeBlockIndex];
 
@@ -34,11 +39,18 @@ class App extends Component {
 
   // Function to get Category Data
   handleCategories(currentPracticeBlock){
-    console.log(currentPracticeBlock);
 
     const leftCategoryID = currentPracticeBlock.fields.leftCategory.sys.id;
 
-    console.log(leftCategoryID);
+    axios.get(`${API_BASE_URL}/spaces/${API_SPACE_ID}/entries?access_token=${API_TOKEN}&content_type=practiceBlock&fields.leftCategory.sys.id=${leftCategoryID}`)
+         .then((response) => {
+          console.log(response);
+          
+            this.setState({ 
+              leftCategoryName : response.data.includes.Entry[1].fields.categoryName,
+              rightCategoryName : response.data.includes.Entry[0].fields.categoryName
+             }); 
+         });
   }
 
   render() {
