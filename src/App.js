d74ce21245146104997383df74475488c6e51e91
Gayan Pathirana <gayan.pathirana@telus.com>
2017-12-04 17:48:33 -0800

    header footer, and form integrated into master.

--- src/App.js
+++ src/App.js
@@ -7,13 +7,8 @@ import Form from './Form.js';
 class App extends Component {
   render() {
     return (
-      <div>
       <div className="App">
         <Header />
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to HRx Bias Test</h1>
-        </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and ssftftstave to reload.
         </p>
