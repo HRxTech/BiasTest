49f62c6c3951205135f6062d844496b75221606f
Azza Rojbi <arojbi@my.bcit.ca>
2017-12-12 15:00:04 -0800

    cleaned warning

--- src/Intro.js
+++ src/Intro.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 import { createClient } from 'contentful';
-import { Link } from 'react-router-dom';
 import FontAwesome from 'react-fontawesome';
 import './Intro.css';
 
