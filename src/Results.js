c42f0304ea992c25e507b91133e51815c31e056c
Parandis <parandis.t.saheli@gmail.com>
2017-12-19 14:20:15 -0800

    style

--- src/Results.js
+++ src/Results.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import './App.css';
+import './Form.css';
 import { ShareButtons,ShareCounts,generateShareIcon } from 'react-share';
 
   const {
